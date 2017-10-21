a74c7644e2ad233229470696bb67eb1db7842f3b
IsraelJr <israel.junior2111@gmail.com>
2017-10-21 16:01:24 -0200

    Projeto com Bound Service - ainda falho

--- app/src/main/java/com/example/israel/demointent/MainActivity.java
+++ app/src/main/java/com/example/israel/demointent/MainActivity.java
@@ -2,15 +2,20 @@ package com.example.israel.demointent;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.israel.demointent.broadcastreceiver.AlarmReceiver;
+import com.example.israel.demointent.services.BoundService;
 import com.example.israel.demointent.utils.Constantes;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
 
     private int contHome = 0;
     private int contVisitante = 0;
+
+    BoundService mBoundService;
+    boolean mServiceBound = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +50,49 @@ public class MainActivity extends AppCompatActivity {
         if(getIntent() != null){
             txtNome.setText(getIntent().getStringExtra(Constantes.KEY_LOGIN));
         }
+
+        final TextView timestampText = (TextView) findViewById(R.id.timestamp_text);
+        Button printTimestampButton = (Button) findViewById(R.id.print_timestamp);
+        Button stopServiceButton = (Button) findViewById(R.id.stop_service);
+
+        printTimestampButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(mServiceBound){
+                timestampText.setText(mBoundService.getTimestamp());
+            }
+        }
+    });
+
+        stopServiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mServiceBound){
+                    unbindService(mServiceConnection);
+                    mServiceBound = false;
+                }
+                Intent intent = new Intent(MainActivity.this, BoundService.class);
+                stopService(intent);
+            }
+        });
+
+
     }
+
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mServiceBound = false;
+        }
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            BoundService.MyBinder myBinder = (BoundService.MyBinder) service;
+            //mBoundService = myBinder.getService();
+            mServiceBound = true;
+        }
+    };
 
     public void golCasa(View v){
         contHome++;
